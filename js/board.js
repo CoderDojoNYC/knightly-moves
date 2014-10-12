f2923371b324b989c41d9b1bcc005475f2272b19
giordan212 <giordan212@hotmail.com>
2014-10-11 20:30:49 -0400

    Attempt to make a button

--- js/board.js
+++ js/board.js
@@ -1,5 +1,22 @@
+  	
+	var board;
+
+
   	var empty = function () {
-		var board = new ChessBoard('board');
+		board = new ChessBoard('board');
   	};
 
   	$(document).ready(empty);
+
+
+  	function makePlayer(board, position){
+  		board.position(position);
+  	}
+
+
+    var example = {
+      a4: 'bK',
+      c4: 'wK',
+      a7: 'wR'
+    };
+    $('#setStartBtn').on('click', makePlayer(board,example));
